5b72e585b56b07c30ea2b64097231b3f23e46a65
ElSenor64 <sillynoodle36@gmail.com>
2020-08-24 11:08:17 -0600

    Added the first repl.it

---
+++ replitz/greeting.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+def greeting(firstName):
+    print("Hello, "+firstName+".")
+    print("My name is Morpheous.")
+    print("Please choose the blue pill or the red pill...")
+
+def pill():
+    pillchoice = input("Please enter \'Red\' or \'Blue\':")
+    if pillchoice == "Red":
+        print("Welcome to the Matrix!")
+    elif pillchoice == "red":
+        print("Welcome to the Matrix!")
+    else:
+        print("Thou art denyest the braves and is put thou head into the sand.")
+
+name = input("What's your name?")
+greeting(name)
+pill()
